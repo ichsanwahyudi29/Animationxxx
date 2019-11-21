eaa52fba21078e0402f244486ee86df95d7fadca
ichsanwahyudi29 <32062159+ichsanwahyudi29@users.noreply.github.com>
2019-11-21 09:12:04 +0700

    Update AnimationViewController.swift

--- Animationxxx/AnimationViewController.swift
+++ Animationxxx/AnimationViewController.swift
@@ -39,6 +39,4 @@ class AnimationViewController: ASViewController<ASDisplayNode> {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
 }
